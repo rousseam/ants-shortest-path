ab703c8be01221a6d9a5f5986c226a9825ddddee
rousseam <matteo.rousseau@live.fr>
2024-03-11 17:39:48 +0100

    feature: main.py + Grid class creation

--- classes/Road.py
+++ classes/Road.py
@@ -3,17 +3,20 @@ from City import City
 
 @dataclass
 class Road:
-    __pheromone_quantity: float
-    __length: float =0
-    __starting_city: City
-    __ending_city : City
+    def __init__(self, city1: City, city2: City):
+        self.__city1 = city1
+        self.__city2 = city2
+        self.__pheromone_quantity = 0
+        self.__length = self.set_length(city1, city2)
 
     def set_length(self): 
-        self.__length = ((self.__starting_city.x_coordinate()-self.__ending_city.x_coordinate())**2 - (self.__starting_city.y_coordinate()-self.__ending_city.y_coordinate())**2)**(1/2)
+        self.__length = ((self.__city1.position.x_coordinate()-self.__city2.position.x_coordinate())**2 - (self.__city1.position.y_coordinate()-self.__city2.position.y_coordinate())**2)**(1/2)
 
     def get_length(self): 
         return self.__length
+
     def set_pheromone_quantity(self,h=0):
         self.__pheromone_quantity += h
+
     def get_pheromone_quantity(self):
         return self.__pheromone_quantity
---
+++ main.py
@@ -0,0 +1,6 @@
+from classes.City import City
+from classes.Road import Road
+from classes.Position import Position
+
+if __name__ == '__main__':
+    city1 = City("1", Position())
